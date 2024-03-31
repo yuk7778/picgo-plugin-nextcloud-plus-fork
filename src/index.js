0c31fcfe59ed0ef9b34d2d2bdc48b6c80fc21764
yuk7778 <bkw0424@gmail.com>
2024-03-31 18:49:27 +0800

    Fix TLS Error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const pluginName = 'nextcloud-plus'
+const uploadedName = 'nextcloud'
+const axios = require('axios');
+
 module.exports = (ctx) => {
   const register = () => {
-    ctx.helper.uploader.register(pluginName, {
+    ctx.helper.uploader.register(uploadedName, {
       handle: uploader,
-      name: 'Nextcloud',
+      name: 'NextCloud图床',
       config: config
     })
 
@@ -21,16 +23,26 @@ module.exports = (ctx) => {
     '.tiff': 'image/tiff'
   }
 
-  const Base64 = require('js-base64').Base64
-
-  // Basic auth
-  const getAuth = function (user, password) {
-    let loginUser = user + ':' + password
-    return "Basic " + Base64.encode(loginUser)
+  const getPubHeaders = function (fileName, extname) {
+    return {
+      'OCS-APIREQUEST': 'true',
+      'User-Agent': 'PicGo',
+      'Accept': 'application/json'
+    }
   }
 
+  const getAuth = function (user, password) {
+    return {
+      'user': user,
+      'password': password,
+      'sendImmediately': true
+    }
+  }
+
+
   const getUserConfig = function () {
-    let userConfig = ctx.getConfig('picBed.' + pluginName)
+	  
+    let userConfig = ctx.getConfig('picBed.nextcloud')
 
     if (!userConfig) {
       throw new Error('请先配置nextcloud上传参数')
@@ -39,31 +51,29 @@ module.exports = (ctx) => {
     const path = userConfig.path
     const user = userConfig.user
     const password = userConfig.password
+
     userConfig['baseUrl'] = `${host}/remote.php/dav/files/${user}/${encodeURI(path)}`
     userConfig['shareUrl'] = `${host}/ocs/v2.php/apps/files_sharing/api/v1/shares`
     userConfig['auth'] = getAuth(user, password)
-    return userConfig
-  }
 
-  const getPubHeaders = function () {
-    let userConfig = getUserConfig()
-    return {
-      'OCS-APIREQUEST': 'true',
-      'User-Agent': 'PicGo',
-      'Accept': 'application/json',
-      'Authorization': userConfig['auth']
-    }
+    return userConfig
   }
 
   const deleteFile = async function (fileName) {
     let userConfig = getUserConfig()
     let headers = getPubHeaders()
     try {
-      await ctx.request({
+      await ctx.Request.request({
+        rejectUnauthorized: false,
         method: 'delete',
         url: `${userConfig['baseUrl']}/${encodeURI(fileName)}`,
+        auth: userConfig['auth'],
         headers: headers
       })
+      // ctx.emit('notification', {
+      //   title: '删除提示',
+      //   body: `已删除文件${fileName}`
+      // })
     } catch(err) {
       ctx.emit('notification', {
         title: '删除失败',
@@ -73,11 +83,7 @@ module.exports = (ctx) => {
   }
 
   const onRemove = async function (files) {
-    let userConfig = getUserConfig()
-    if (!userConfig.syncDel){
-      return
-    }
-    const rms = files.filter(each => each.type === pluginName)
+    const rms = files.filter(each => each.type === uploadedName)
     if (rms.length === 0) {
       return
     }
@@ -92,79 +98,89 @@ module.exports = (ctx) => {
     }
 
     if (fail.length) {
-      const uploaded = ctx.getConfig('uploaded') || []
+      const uploaded = ctx.getConfig('uploaded')
       uploaded.unshift(...fail)
       ctx.saveConfig(uploaded)
     }
+
+    // ctx.emit('notification', {
+    //   title: '删除提示',
+    //   body: fail.length === 0 ? '成功同步删除' : `删除失败${fail.length}个`
+    // })
   }
 
   const uploader = async function (ctx) {
-    let userConfig = getUserConfig()
+    let userConfig = getUserConfig();
 
     try {
-      let imgList = ctx.output
-      for (let i in imgList) {
-        let image = imgList[i]
-        let data = image.buffer
-        if (!data && image.base64Image) {
-          data = Buffer.from(image.base64Image, 'base64')
-        }
-        let headers = getPubHeaders()
-        const contentType = mimeTypes[image.extname] || 'application/octet-stream'
-        await ctx.request({
-          method: 'put',
-          url: `${userConfig['baseUrl']}/${encodeURI(image.fileName)}`,
-          headers: {
-            ...headers,
-            'Content-Disposition': `attachment; filename="${encodeURI(image.fileName)}"`,
-            'Content-Type': contentType
-          },
-          body: data
-        })
+		axios.defaults.auth = {
+			username: userConfig.user,
+			password: userConfig.password};
+        let imgList = ctx.output;
+        for (let i in imgList) {
+            let image = imgList[i];
+            let data = image.buffer;
+            if (!data && image.base64Image) {
+                data = Buffer.from(image.base64Image, 'base64');
+            }
+            let headers = getPubHeaders();
+            const contentType = mimeTypes[image.extname] || 'application/octet-stream';
+            
+            await axios.put(`${userConfig['baseUrl']}/${encodeURI(image.fileName)}`, data, {
+                headers: {
+                    ...headers,
+                    'Content-Disposition': `attachment; filename="${encodeURI(image.fileName)}"`,
+                    'Content-Type': contentType
+                },
+                https: {
+                    minVersion: 'TLSv1.2' // 指定最低支持的TLS版本
+                }
+            });
 
-        let body = await ctx.request({
-          method: 'post',
-          url: userConfig['shareUrl'],
-          headers: headers,
-          formData: {
-            path: `${userConfig.path}/${image.fileName}`,
-            shareType: 3
-          }
-        })
-        delete image.base64Image
-        delete image.buffer
-        body = JSON.parse(body).ocs
-        if (body.meta.statuscode === 200) {
-          image.imgUrl = body.data.url + '/preview'
-          ctx.emit('notification', {
-            title: '图片链接已复制！',
-            body: ''
-          })
-        } else {
-          ctx.emit('notification', {
-            title: 'Nextcloud共享失败',
-            body: '请检查Nextcloud设置'
-          })
-          deleteFile(image.fileName)
+            let body = await axios.post(userConfig['shareUrl'], {
+                path: `${userConfig.path}/${image.fileName}`,
+                shareType: 3
+            }, {
+                headers: headers,
+                https: {
+                    minVersion: 'TLSv1.2' // 指定最低支持的TLS版本
+                }
+            });
+
+            delete image.base64Image;
+            delete image.buffer;
+            body = body.data.ocs;
+            if (body.meta.statuscode === 200) {
+                image.imgUrl = body.data.url + '/preview';
+                ctx.emit('notification', {
+                    title: '图片链接已复制！',
+                    body: ''
+                });
+            } else {
+                ctx.emit('notification', {
+                    title: 'Nextcloud共享失败',
+                    body: '请检查Nextcloud设置'
+                });
+                deleteFile(image.fileName);
+            }
         }
-      }
     } catch (err) {
-      if (err.message.indexOf('404') === 0) {
-        ctx.emit('notification', {
-          title: '上传失败',
-          body: '路径不存在，请检查路径设置'
-        })
-      } else {
-        ctx.emit('notification', {
-          title: '上传失败',
-          body: err.message
-        })
-        ctx.log.error('', e)
-      }
+        if (err.message.indexOf('404') === 0) {
+            ctx.emit('notification', {
+                title: '上传失败',
+                body: '路径不存在，请检查路径设置'
+            });
+        } else {
+            ctx.emit('notification', {
+                title: '上传失败',
+                body: err.message
+            });
+            ctx.log.error('', err);
+        }
     }
-  }
+}
   const config = ctx => {
-    let userConfig = ctx.getConfig('picBed.' + pluginName)
+    let userConfig = ctx.getConfig('picBed.nextcloud')
     if (!userConfig) {
       userConfig = {}
     }
@@ -174,45 +190,37 @@ module.exports = (ctx) => {
         type: 'input',
         default: userConfig.host,
         required: true,
-        message: 'Nextcloud service address',
-        alias: 'service'
+        message: '服务地址',
+        alias: '服务地址'
       },
       {
         name: 'user',
         type: 'input',
         default: userConfig.user,
         required: true,
-        message: 'username',
-        alias: 'username'
+        message: '用户名',
+        alias: '用户名'
       },
       {
         name: 'password',
         type: 'password',
         default: userConfig.password,
         required: true,
-        message: 'password',
-        alias: 'password'
+        message: '密码',
+        alias: '密码'
       },
       {
         name: 'path',
         type: 'input',
         default: userConfig.path,
         required: true,
-        message: 'path',
-        alias: 'path'
-      },
-      {
-        name: 'syncDel',
-        type: 'confirm',
-        default: userConfig.syncDel,
-        required: true,
-        message: 'sync delete',
-        alias: 'sync del'
+        message: '自定义保存路径',
+        alias: '保存路径'
       }
     ]
   }
   return {
-    uploader: pluginName,
+    uploader: 'nextcloud',
     register
   }
 }
